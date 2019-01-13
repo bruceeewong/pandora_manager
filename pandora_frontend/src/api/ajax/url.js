aea9f4fa6432ec9cb002276f5856ba4eb4f2d7fe
unknown <wangsizhe@tuhu.ad>
2019-01-13 16:53:48 +0800

    modify url

--- pandora_frontend/src/api/ajax/url.js
+++ pandora_frontend/src/api/ajax/url.js
@@ -1,4 +1,5 @@
-const baseUrl = 'http://localhost:3000'
+// const baseUrl = 'http://localhost:3000'
+const baseUrl = 'http://120.79.15.25:3000'
 
 export default {
   getAllFiles: `${baseUrl}/api/getallfiles`,
